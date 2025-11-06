37694dc134b64a1384c0b8f86239a1c627defac0
Heisen111 <devarshidave007@gmail.com>
2025-11-06 09:06:23 +0000

    Added ERC-20 tokens unit tests

--- test/unit/TimeLockedVault.t.sol
+++ test/unit/TimeLockedVault.t.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.20;
 import {TimeLockedVault} from "src/TimeLockedVault.sol";
 import {Test} from "forge-std/Test.sol";
 import {DeployTimeLockedVault} from "script/DeployTimeLockedVault.s.sol";
+import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
 
 contract TimeLockedVaultTest is Test {
     TimeLockedVault timeLockedVault;
+    ERC20Mock token;
 
     address USER = makeAddr("user");
     uint256 UNLOCKTIME = block.timestamp + 5 days;
@@ -16,6 +18,11 @@ contract TimeLockedVaultTest is Test {
         DeployTimeLockedVault deployTimeLockedVault = new DeployTimeLockedVault();
         timeLockedVault = deployTimeLockedVault.run();
         vm.deal(USER, STARTING_VALUE);
+        token = new ERC20Mock();
+        token.transfer(USER, 500 ether);
+        vm.startPrank(USER);
+        token.approve(address(timeLockedVault), type(uint256).max);
+        vm.stopPrank();
     }
 
     function testOwnerIsMsgSender() public view {
@@ -112,5 +119,67 @@ contract TimeLockedVaultTest is Test {
         timeLockedVault.extendEthLock(newUnlockTime);
         (, uint256 unlockTime) = timeLockedVault.getEthLock(USER);
         assertEq(unlockTime, newUnlockTime);
+    }
+
+    function testDepositTokenWorks() public {
+        vm.prank(USER);
+        timeLockedVault.depositToken(address(token), 100 ether, UNLOCKTIME);
+
+        (uint256 amount, uint256 unlock) = timeLockedVault.tokenLocks(USER, address(token));
+
+        assertEq(amount, 100 ether);
+        assertEq(unlock, UNLOCKTIME);
+        assertEq(token.balanceOf(address(timeLockedVault)), 100 ether);
+    }
+
+    function testWithdrawTokenWorks() public {
+        vm.prank(USER);
+        timeLockedVault.depositToken(address(token), 100 ether, UNLOCKTIME);
+        vm.warp(UNLOCKTIME + 1);
+
+        vm.prank(USER);
+        timeLockedVault.withdrawToken(address(token));
+        (uint256 amount,) = timeLockedVault.tokenLocks(USER, address(token));
+        assertEq(amount, 0);
+    }
+
+    function testExtendTokenLockWorks() public {
+        vm.prank(USER);
+        timeLockedVault.depositToken(address(token), 100 ether, UNLOCKTIME);
+
+        uint256 newUnlockTime = UNLOCKTIME + 3 days;
+        vm.prank(USER);
+        timeLockedVault.extendTokenLock(address(token), newUnlockTime);
+        (, uint256 unlocktime) = timeLockedVault.tokenLocks(USER, address(token));
+        assertEq(unlocktime, newUnlockTime);
+    }
+
+    function testTransferOwnershipWorks() public {
+        address newOwner = makeAddr("newOwner");
+        address owner = timeLockedVault.owner();
+
+        vm.prank(owner);
+        timeLockedVault.transferOwnership(newOwner);
+
+        assertEq(timeLockedVault.owner(), newOwner);
+    }
+
+    function testTransferOwnershipFailsWithInvalidNewOwner() public {
+        address owner = timeLockedVault.owner();
+
+        vm.prank(owner);
+        vm.expectRevert("Invalid new owner");
+        timeLockedVault.transferOwnership(address(0));
+    }
+
+    function testEmergencyWithdrawWorks() public {
+        vm.prank(USER);
+        timeLockedVault.depositToken(address(token), 100 ether, UNLOCKTIME);
+
+        address owner = timeLockedVault.owner();
+        vm.prank(owner);
+        timeLockedVault.emergencyWithdraw(USER, address(token));
+
+        assertEq(token.balanceOf(USER), 500 ether);
     }
 }
